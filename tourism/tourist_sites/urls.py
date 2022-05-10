b6057cdabc30c74de8e98eb0f8634fffb9e8219e
zainab <zeinab@kuwaitnet.dev>
2022-05-10 09:29:17 +0300

    site details back-end

--- tourism/tourist_sites/urls.py
+++ tourism/tourist_sites/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TouristSitesView
+from .views import TouristSitesView, TouristSiteDetail
 
 app_name = "tourist_sites"
 
 urlpatterns = [
     path("", TouristSitesView.as_view(), name="index"),
+    path("site-details/<int:pk>", TouristSiteDetail.as_view(), name="site-details"),
 ]
--- tourism/tourist_sites/views.py
+++ tourism/tourist_sites/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
-from .models import City
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import City, TouristPlace
 
 
 class TouristSitesView(ListView):
     model = City
     template_name = 'tourist_sites/tourist_sites.html'
+
+
+class TouristSiteDetail(DetailView):
+    model = TouristPlace
+    template_name = 'tourist_sites/tourist_site_detail.html'
 
